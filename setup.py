fa8ce9688b2d11947d90edce4e3c4e0ce4ec4b49
Paul Garner <paul@potatolondon.com>
2012-12-07 00:11:30 +0000

    more pypi tweaks

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='EbaySuds',
-    version='0.2.4',
+    version='0.2.5',
     packages=['ebaysuds',],
     license='LGPL v3',
     long_description=open('pypi.rst').read(),
