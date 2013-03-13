b921792a51d1659ced6d36d0722c46e802ddcafd
Paul Garner <paul@potatolondon.com>
2013-03-13 00:07:34 +0000

    remove dummy tests created by django

--- ebaysuds/tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-class SimpleTest(unittest.TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
