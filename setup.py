6b76d674d2e80fa761325d9700a2c1df048f5139
anentropic <ego@anentropic.com>
2013-03-15 18:04:20 +0000

    pypi version 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='EbaySuds',
-    version='0.3.4',
+    version='0.4.0',
     packages=['ebaysuds',],
     license='LGPL v3',
     long_description=open('pypi.rst').read(),
