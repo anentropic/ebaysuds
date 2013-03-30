c7738b9351901f673c0b2f0dc27a642bc3c7cd1c
anentropic <ego@anentropic.com>
2013-03-30 23:09:30 +0000

    function to recursively dict-ify suds objects

--- ebaysuds/utils.py
+++ ebaysuds/utils.py
@@ -2,6 +2,9 @@ import os
 import shutil
 from tempfile import gettempdir as tmp
 
+from suds.sudsobject import asdict
+
+
 def clear_cache():
 	"""
 	Clear the suds object / wsdl cache
@@ -9,3 +12,25 @@ def clear_cache():
     http://pythonaut.blogspot.co.uk/2011/10/how-to-clear-suds-cache.html
 	"""
     shutil.rmtree(os.path.join(tmp(), 'suds'), True)
+
+
+def recursive_asdict(d):
+    """
+    Recursively convert Suds object into dict.
+
+    http://stackoverflow.com/a/15678861/202168
+    """
+    out = {}
+    for k, v in asdict(d).iteritems():
+        if hasattr(v, '__keylist__'):
+            out[k] = recursive_asdict(v)
+        elif isinstance(v, list):
+            out[k] = []
+            for item in v:
+                if hasattr(item, '__keylist__'):
+                    out[k].append(recursive_asdict(item))
+                else:
+                    out[k].append(item)
+        else:
+            out[k] = v
+    return out
